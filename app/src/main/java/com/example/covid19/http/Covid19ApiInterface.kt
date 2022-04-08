e6b2b3e58e5babca05ebdf37d8620b395bb983b6
Arbon Vata <Arbon.Vata@gmail.com>
2022-04-08 22:57:35 +0200

    Fixed library updating

--- app/src/main/java/com/example/covid19/http/Covid19ApiInterface.kt
+++ app/src/main/java/com/example/covid19/http/Covid19ApiInterface.kt
@@ -5,23 +5,31 @@ import com.example.covid19.CountryStatistic.CountryStatisticData
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import io.ktor.client.HttpClient
-import io.ktor.client.request.get
+import io.ktor.client.call.body
+import io.ktor.client.request.request
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpMethod
 
 // see https://developer.android.com/training/basics/network-ops/connecting
 object Covid19ApiInterface {
 
-    private val client = HttpClient()
+    private val client = HttpClient() {
+    }
     private val gson = Gson()
 
     suspend fun getCountryList(): ArrayList<Country> {
-        val response = client.get<String>("https://api.covid19api.com/countries")
+        val response = client.request("https://api.covid19api.com/countries") {
+            method = HttpMethod.Get
+        }
         val itemType = object : TypeToken<ArrayList<Country>>() {}.type
-        return gson.fromJson(response, itemType)
+        return gson.fromJson(response.body<String>(), itemType)
     }
 
     suspend fun getCountryStatistic(countryId: String): ArrayList<CountryStatisticData> {
-        val response = client.get<String>("https://api.covid19api.com/country/$countryId")
+        val response = client.request("https://api.covid19api.com/country/$countryId") {
+            method = HttpMethod.Get
+        }
         val itemType = object : TypeToken<ArrayList<CountryStatisticData>>() {}.type
-        return gson.fromJson(response, itemType)
+        return gson.fromJson(response.bodyAsText(), itemType)
     }
 }
